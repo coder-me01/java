00b8c7ebeb145f80b9ff120e75351fe7b1f5e92b
Kartik Bhat <76040124+coder-me01@users.noreply.github.com>
2024-05-19 22:55:07 +0530

    prgrm_10.java

---
+++ prgrm_10.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+ import java.applet.*;
+ public class Checkerboard extends Applet {
+ public void paint(Graphics g) {
+ int row; // Row number, from 0 to 7
+ int col; // Column number, from 0 to 7
+ int x,y; // Top-left corner of square
+ for ( row = 0; row < 8; row++ ) {
+ for ( col = 0; col < 8; col++) {
+ x = col * 20;
+ y = row * 20;
+ if ( (row % 2) == (col % 2) )
+ g.setColor(Color.red);
+ else
+ g.setColor(Color.black);
+ g.fillRect(x, y, 20, 20);
+ }
+ } // end for row
+ } // end paint()
+ } // end class
